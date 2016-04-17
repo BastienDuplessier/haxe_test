065d15fe0ab11635797080a987b49be0fa73a2df
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-04-17 18:29:43 +0200

    Collisions

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,11 +3,12 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxObject;
+import flixel.group.FlxGroup;
 
 class PlayState extends FlxState
 {
-	public var players:Array<Player>;
-	public var map:Array<FlxObject>;
+	public var players:FlxGroup;
+	public var map:FlxGroup;
 
 	override public function create():Void 
 	{
@@ -17,18 +18,29 @@ class PlayState extends FlxState
 
 		bgColor = 0xffaaaaaa;
 
-		players = new Array<Player>();
-		players.push(new DebugPlayer());
+		players = new FlxGroup();
+		players.add(new DebugPlayer());
 		for(player in players) { add(player); }
-		map = new Array<FlxObject>();
-		var ground = new FlxObject(0, 500, 1024, 100);
-		map.push(ground);
-		add(ground);
+		map = new FlxGroup();
+		map.add(new FlxObject(0, 416, 300, 184));
+		map.add(new FlxObject(352.25, 416, 208, 184));
+		map.add(new FlxObject(560.25, 379, 115, 221));
+		map.add(new FlxObject(675.25, 357, 46, 243));
+		map.add(new FlxObject(777, 370,247, 230));
+		for(elt in map) { cast(elt, FlxObject).immovable = true; add(elt); }
 	}
-	
+
 	override public function update(elapsed:Float):Void 
 	{
 		super.update(elapsed);
+		update_players(elapsed);
+	}
+
+	public function update_players(elapsed:Float):Void {
+		for(player in players) {
+			player.update(elapsed);
+			FlxG.collide(map, player, FlxObject.separate);
+		}
 	}
 
 }
