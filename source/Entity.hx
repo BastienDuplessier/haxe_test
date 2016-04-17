dd705910c09ca029190f8f6ffbe0c45b909d2f05
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-04-17 08:16:45 +0200

    Fix y collisions

--- source/Entity.hx
+++ source/Entity.hx
@@ -24,7 +24,7 @@ class Entity {
 		height = 38;
 		
 		sprite = new FlxSprite();
-		sprite.loadGraphic("assets/sprites/coin.png");
+		sprite.makeGraphic(25, 38, 0xffaa1111);
 	}
 	
 	public function setCoordinates(x,y):Void {
@@ -38,7 +38,7 @@ class Entity {
 	}
 
 	public function collide_with_border(cx, cy):Bool {
-		return cx<0 || cx>=1024 || cy>600;
+		return cx<0 || cx>=1024 || cy>=600;
 	}
 	
 	public function collide_with_map(cx, cy, rects:Array<Array<Float>>):Bool { 
@@ -53,36 +53,61 @@ class Entity {
 	}
 
 	public function onGround():Bool {
-		return hasCollision(cx,cy+1, []);
+		return hasCollision(cx,cy+Std.int(height)+1, []);
+	}
+
+	public function adjust_position_x(cx, cy, xx, rects):Int {
+		var ix = xx;
+		while(hasCollision(ix, cy, rects) || cx == ix) {
+			if(cx > xx) {ix--;} else {ix++;}
+		}
+		return Std.int(ix); 
+	}
+
+	public function adjust_position_y(cx, cy, yy, rects):Int {
+		var iy = yy;
+		while(hasCollision(cx, iy, rects) || cy == iy) {
+			if(cy < yy) {iy--;} else {iy++;}
+		}
+		return Std.int(iy); 
 	}
 	
 	public function update(rects):Void {
 		var frictX = 0.92;
 		var frictY = 0.94;
 		var gravity = 0.4;
-
 		// X component
-		// dx*=frictX;
+		dx*=frictX;
+		xx = cx + Math.floor(dx);
+		// Left colliding ?
+		// if(hasCollision(xx, cy, rects)) {
+		// 	yy = 0;
+		// 	dy = 0;
+		// }
+		// // Bottom colliding ?
+		// if(hasCollision(cx, Math.floor(yy + height), rects)) {
+		// 	yy = 600 - height;
+		// 	dy = 0;
+		// }
 
 		// Y component
 		dy+=gravity;
 		dy*=frictY;
-		trace("dy");
-		trace(dy);
 		yy = cy + Math.floor(dy);
-		trace("yy");
-		trace(yy);
-
+		// Top colliding ?
+		if(hasCollision(cx, Math.floor(yy), rects)) {
+			dy = gravity;
+			yy = adjust_position_y(cx, cy, Math.floor(yy), rects);
+		}
 		// Bottom colliding ?
 		if(hasCollision(cx, Math.floor(yy + height), rects)) {
-			trace("yo");
-			yy = 600 - height;
-			dy = 0;
+			dy = gravity;
+			yy = adjust_position_y(cx, cy, Math.floor(yy + height), rects) - height;
 		}
 
-		// sprite.x = xx;
+		sprite.x = xx;
 		sprite.y = yy;
-		// cx = Math.round(xx);
+		cx = Math.round(xx);
 		cy = Math.round(yy);
 	}
 }
