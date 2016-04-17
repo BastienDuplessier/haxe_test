d91b3b3384876bd027cd43fe3cc63c0b745f0681
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-04-18 01:30:36 +0200

    Add borders colliders

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -15,18 +15,20 @@ class PlayState extends FlxState
 		FlxG.mouse.visible = false;
 		// ONLY ON TEST
 		FlxG.debugger.visible = true;
-
 		bgColor = 0xffaaaaaa;
 
 		players = new FlxGroup();
 		players.add(new DebugPlayer());
 		for(player in players) { add(player); }
 		map = new FlxGroup();
-		map.add(new FlxObject(0, 416, 300, 184));
-		map.add(new FlxObject(352.25, 416, 208, 184));
-		map.add(new FlxObject(560.25, 379, 115, 221));
-		map.add(new FlxObject(675.25, 357, 46, 243));
-		map.add(new FlxObject(777, 370,247, 230));
+		// map.add(new FlxObject(0, 416, 300, 184));
+		// map.add(new FlxObject(352.25, 416, 208, 184));
+		// map.add(new FlxObject(560.25, 379, 115, 221));
+		// map.add(new FlxObject(675.25, 357, 46, 243));
+		// map.add(new FlxObject(777, 370,247, 230));
+		map.add(new FlxObject(0, FlxG.height - 100, FlxG.width, 100));
+		map.add(new FlxObject(-10, -200, 10, 1000));
+		map.add(new FlxObject(FlxG.width, -200, 10, 1000));
 		for(elt in map) { cast(elt, FlxObject).immovable = true; add(elt); }
 	}
 
