d53ca64ce7c7715326c10613b42807459bc96be1
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-04-17 17:07:41 +0200

    Add player and ground

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,9 +2,12 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxObject;
 
 class PlayState extends FlxState
 {
+	public var players:Array<Player>;
+	public var map:Array<FlxObject>;
 
 	override public function create():Void 
 	{
@@ -13,6 +16,14 @@ class PlayState extends FlxState
 		FlxG.debugger.visible = true;
 
 		bgColor = 0xffaaaaaa;
+
+		players = new Array<Player>();
+		players.push(new DebugPlayer());
+		for(player in players) { add(player); }
+		map = new Array<FlxObject>();
+		var ground = new FlxObject(0, 500, 1024, 100);
+		map.push(ground);
+		add(ground);
 	}
 	
 	override public function update(elapsed:Float):Void 
--- source/Player.hx
+++ source/Player.hx
@@ -1,10 +1,20 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.FlxObject;
 
 class Player extends FlxSprite {
 
-	public function new():Void {
+	private var name:String;
+
+	public function new(asset):Void {
+		super();
+		maxVelocity.x = 160;
+		maxVelocity.y = 400;
+		acceleration.y = 400;
+		// drag.x = player.maxVelocity.x * 4;
+	}
+
 	}
 	
 }
