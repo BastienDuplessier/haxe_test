cd48b44af7987a18cee3ed78b1b4077b6708567c
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-04-18 01:31:21 +0200

    Debug controls

--- source/DebugPlayer.hx
+++ source/DebugPlayer.hx
@@ -11,5 +11,21 @@ class DebugPlayer extends Player {
 		screenCenter();
 		x = Std.random(FlxG.width);
 	}
+
+	public override function update(elapsed:Float) {
+		super.update(elapsed);
+		if (FlxG.keys.anyPressed([LEFT, A]))
+		{
+			velocity.x -= maxVelocity.x / 10;
+		} else if (FlxG.keys.anyPressed([RIGHT, D]))
+		{
+			velocity.x += maxVelocity.x / 10;
+		} else
+		{
+			var res = velocity.x / 2;
+			velocity.x = res > maxVelocity.x/200 ? res : 0;
+		} 
+	}
+	
 	
 }
