8c137cb71126e7bf34ea358da08b4d2e89ace20b
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-05-06 13:54:10 +0200

    Fix indent

--- source/Robot.hx
+++ source/Robot.hx
@@ -6,9 +6,9 @@ import flixel.FlxG;
 
 class Robot extends FlxSprite {
 
-        public var jumping:Bool;
-        public var falling:Bool;
-        private var lastVelocityY:Float;
+  public var jumping:Bool;
+  public var falling:Bool;
+  private var lastVelocityY:Float;
 
 	public function new(sprSht:String):Void {
 		super();
@@ -22,50 +22,50 @@ class Robot extends FlxSprite {
 		acceleration.y = 400;
 		jumping = false;
 		falling = false;
-                lastVelocityY = 0;
-                FlxG.watch.add(velocity, "y");
-                FlxG.watch.add(this, "jumping");
-                FlxG.watch.add(this, "falling");
-                FlxG.watch.add(this, "lastVelocityY");
+    lastVelocityY = 0;
+    FlxG.watch.add(velocity, "y");
+    FlxG.watch.add(this, "jumping");
+    FlxG.watch.add(this, "falling");
+    FlxG.watch.add(this, "lastVelocityY");
 	}
 
 	private function initSpriteSheet(sprSht:String) {
 	}
 
 	public override function update(elapsed:Float) {
-                updatePhysics();
+    updatePhysics();
 		super.update(elapsed);
 	}
 
-        // Robot was on the ground
-        public function jump() {
-                falling = false;
-                jumping = true;
-        }
+  // Robot was on the ground
+  public function jump() {
+    falling = false;
+    jumping = true;
+  }
 
-        // Robot is jumping
+  // Robot is jumping
 	public function ascend() {
- 	        if(!falling) {
-                        velocity.y -= 50;
-                        if(velocity.y == -maxVelocity.y)
-                                fall();
-                }
+ 	  if(!falling) {
+      velocity.y -= 50;
+      if(velocity.y == -maxVelocity.y)
+        fall();
+    }
 	}
 
-        private function fall() {
-                falling = true;
-                jumping = false;
-        }
+  private function fall() {
+    falling = true;
+    jumping = false;
+  }
 
-        // Update physics on robot
-        private function updatePhysics() {
-                if(velocity.y == 0) {
-                        falling = false;
-                }
-                if(jumping && (lastVelocityY < velocity.y)) {
-                        falling = true;
-                        jumping = false;
-                }
-                lastVelocityY = velocity.y;       
-        }
+  // Update physics on robot
+  private function updatePhysics() {
+    if(velocity.y == 0) {
+      falling = false;
+    }
+    if(jumping && (lastVelocityY < velocity.y)) {
+      falling = true;
+      jumping = false;
+    }
+    lastVelocityY = velocity.y;
+  }
 }
