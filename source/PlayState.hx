ae372bf4d035aa25ecd25bc5e68febdae9cfe3c8
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-05-06 13:51:12 +0200

    Change layout

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -21,12 +21,12 @@ class PlayState extends FlxState
 		robots.add(new DebugRobot());
 		for(robot in robots) { add(robot); }
 		map = new FlxGroup();
-		// map.add(new FlxObject(0, 416, 300, 184));
-		// map.add(new FlxObject(352.25, 416, 208, 184));
-		// map.add(new FlxObject(560.25, 379, 115, 221));
-		// map.add(new FlxObject(675.25, 357, 46, 243));
-		// map.add(new FlxObject(777, 370,247, 230));
-		map.add(new FlxObject(0, FlxG.height - 100, FlxG.width, 10000));
+		map.add(new FlxObject(0, 416, 300, 184));
+		map.add(new FlxObject(352.25, 416, 208, 184));
+		map.add(new FlxObject(560.25, 379, 115, 221));
+		map.add(new FlxObject(675.25, 357, 46, 243));
+		map.add(new FlxObject(777, 370,247, 230));
+		// map.add(new FlxObject(0, FlxG.height - 100, FlxG.width, 10000));
 		map.add(new FlxObject(-10, -200, 10, 1000));
 		map.add(new FlxObject(FlxG.width, -200, 10, 1000));
 		for(elt in map) { cast(elt, FlxObject).immovable = true; add(elt); }
