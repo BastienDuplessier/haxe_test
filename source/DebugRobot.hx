08a991adecd2df58f2a0b2139c6f9afeef9c1bb7
Bastien Duplessier <bastien.duplessier@gmail.com>
2016-05-06 14:04:10 +0200

    Fix indent

--- source/DebugRobot.hx
+++ source/DebugRobot.hx
@@ -4,35 +4,31 @@ import flixel.util.FlxColor;
 import flixel.FlxG;
 
 class DebugRobot extends Robot {
+  public function new():Void {
+    super("");
+    makeGraphic(25,38, FlxColor.BLUE);
+    screenCenter();
+    x = Std.random(FlxG.width);
+  }
 
-	public function new():Void {
-		super("");
-		makeGraphic(25,38, FlxColor.BLUE);
-		screenCenter();
-		x = Std.random(FlxG.width);
-	}
-
-	public override function update(elapsed:Float) {
-                // Horizontal movement
-		if (FlxG.keys.anyPressed([LEFT, A])) {
-			velocity.x -= maxVelocity.x / 10;
-		} else if (FlxG.keys.anyPressed([RIGHT, D])) {
-			velocity.x += maxVelocity.x / 10;
-		} else {
-			var res = velocity.x / 2;
-			velocity.x = res > maxVelocity.x/200 ? res : 0;
-		}
-                // Vertical movement
-                if (FlxG.keys.anyPressed([SPACE, UP])) {
-                        if(!falling) {
-                                if(!jumping) {
-                                        jump();
-                                }
-                                ascend();
-                        }
-                }
-		super.update(elapsed);
-	}
-	
-	
+  public override function update(elapsed:Float) {
+    // Horizontal movement
+    if (FlxG.keys.anyPressed([LEFT, A])) {
+      velocity.x -= maxVelocity.x / 10;
+    } else if (FlxG.keys.anyPressed([RIGHT, D])) {
+      velocity.x += maxVelocity.x / 10;
+    } else {
+      var res = velocity.x / 2;
+      velocity.x = res > maxVelocity.x/200 ? res : 0;
+    }
+    // Vertical movement
+    if (FlxG.keys.anyPressed([SPACE, UP])) {
+      if(!falling) {
+        if(!jumping)
+          jump();
+        ascend();
+      }
+    }
+    super.update(elapsed);
+  }
 }
